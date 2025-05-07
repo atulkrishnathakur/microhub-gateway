0feaae3a8d212f497244af198160a63700ebe817
Atul <gitvcs@gmail.com>
2025-05-07 10:28:25 +0530

    docker compose work

--- Dockerfile
+++ Dockerfile
@@ -14,4 +14,7 @@ RUN pip install --no-cache-dir --upgrade -r /microhub-gateway/requirements.txt
 COPY ./app /microhub-gateway/app
 
 # this is default command. It will run after container start
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+# production
+# CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000", "--workers", "4"]
+# development
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000","--reload"]
--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import httpx
+from pydantic import (BaseModel,Field, model_validator, EmailStr, ModelWrapValidatorHandler, ValidationError, AfterValidator,BeforeValidator,PlainValidator, ValidatorFunctionWrapHandler)
+from typing import List
 
 app = FastAPI()
 
@@ -11,17 +13,62 @@ MICROSERVICES = {
 def root():
     return {"message":"hello world sss"}
 
-@app.get("/user-management/sa1")
-async def sa():
+@app.get("/abc")
+def root():
+    return {"message":"hello world abc test qqqq abc"}
+
+
+class EmpSchemaIn(BaseModel):
+    emp_name: str = Field(example="Atul")
+    email: EmailStr = Field(example="atul@comsysapp.com")
+    mobile: str | None = Field(example="000000")
+    status: int | None = Field(default=1)
+    password: str = Field(example="aa")
+    confirm_password:str = Field(example="aa")
+    
+@app.post("/user-management/emp-save")
+async def empsave(empm: EmpSchemaIn):
     async with httpx.AsyncClient() as client:
         try:
-            response = await client.get(f"{MICROSERVICES['user_management']}/a1", timeout=5.0)
+            url = f"{MICROSERVICES['user_management']}/api/emp-save"
+            payload = {"emp_name": "Atul","email": "atul9@yopmail.com","mobile": "000000","status": 1,"password": "aa","confirm_password": "aa"}
+            response = await client.post('http://microhubusermanagementcontainer:8000/api/a1',json=payload, timeout=None)
             response.raise_for_status()
             return response.json()
         except httpx.ConnectError:
-            return {"error": "Microservice reachable nahi hai!"}
+            return {"error": "Microservice is not reachable"}
         except httpx.HTTPStatusError as exc:
             return {"error": f"Microservice error: {exc.response.status_code}"}
+
+@app.post("/user-management/a1")
+async def a1():
+    async with httpx.AsyncClient() as client:
+        try:
+            url = f"{MICROSERVICES['user_management']}/api/emp-save"
+            reqjson = {"emp_name": "Atul","email": "atul9@yopmail.com","mobile": "000000","status": 1,"password": "aa","confirm_password": "aa"}
+            response = await client.get('http://microhubusermanagementcontainer:8000/api/a1', timeout=None)
+            response.raise_for_status()
+            return response.json()
+        except httpx.ConnectError:
+            return {"error": "Microservice is not reachable"}
+        except httpx.HTTPStatusError as exc:
+            return {"error": f"Microservice error: {exc.response.status_code}"}
+
+@app.post("/user-management/posta1")
+async def a1():
+    async with httpx.AsyncClient() as client:
+        try:
+            url = f"{MICROSERVICES['user_management']}/api/emp-save"
+            payload = {"emp_name": "Atul","email": "atul9@yopmail.com","mobile": "000000","status": 1,"password": "aa","confirm_password": "aa"}
+            response = await client.post('http://microhubusermanagementcontainer:8000/api/posta1', timeout=None)
+            response.raise_for_status()
+            return response.json()
+        except httpx.ConnectError:
+            return {"error": "Microservice is not reachable"}
+        except httpx.HTTPStatusError as exc:
+            return {"error": f"Microservice error: {exc.response.status_code}"}
+
+
 
 
 @app.get("/user-management/sa2")
