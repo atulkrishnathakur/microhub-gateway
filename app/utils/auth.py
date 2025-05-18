3f99e2deae92009800e2cd4a4e102598d0549221
Atul <gitvcs@gmail.com>
2025-05-19 03:00:14 +0530

    auth work

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -2,31 +2,11 @@ from typing import Annotated
 from fastapi import Depends, status
 import jwt
 from sqlalchemy.orm import Session
-from app.database.model_functions.login import get_emp_for_login
 from fastapi import HTTPException, Response, Request
 from app.utils.hashing import HashData
 from app.config.message import auth_message
 from app.validation.auth import TokenData
 from app.utils.httpbearer import get_api_token
-
-def authenticate(email,password,db):
-    dbempm = get_emp_for_login(db,email)
-    if not dbempm:
-        raise CustomException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            status=False,
-            message=auth_message.INCORRECT_CREDENTIALS,
-            data=[]
-        )
-        
-    if not HashData.verify_password(password, dbempm.password):
-        raise CustomException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            status=False,
-            message=auth_message.INCORRECT_CREDENTIALS,
-            data=[]
-        )   
-    return dbempm
 
 async def getCurrentEmp(token: Annotated[str, Depends(get_api_token)], db: Annotated[Session, Depends(get_db)]):
     if token in blacklist:
